00f39d6e702d4a75d8a51c8bd0827527a3d694b6
danielosorio0224 <dosorioquimbayo@gmail.com>
2024-06-21 15:51:02 -0500

    saddle_points

---
+++ saddle_points.js
@@ -0,0 +1,9 @@
+export const saddlePoints = (matrix) => {
+  return matrix.flatMap((row, y) =>
+    row.flatMap((val, x) =>
+      row.every((v) => val >= v) && matrix.every((r) => val <= r[x])
+        ? { row: y + 1, column: x + 1 }
+        : []
+    )
+  );
+};
