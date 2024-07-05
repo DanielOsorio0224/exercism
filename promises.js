2673f1e8a17960c3359ebed22f9e94d1246843cf
danielosorio0224 <dosorioquimbayo@gmail.com>
2024-07-05 16:53:46 -0500

    promises

---
+++ promises.js
@@ -0,0 +1,44 @@
+export const promisify =
+  (fn) =>
+  (...args) =>
+    new Promise((resolve, reject) =>
+      fn(...args, (err, result) => (err ? reject(err) : resolve(result)))
+    );
+
+export const all = (promises) =>
+  promises
+    ? promises.reduce(
+        async (acc, promise) => (await acc).concat(await promise),
+        Promise.resolve([])
+      )
+    : Promise.resolve();
+
+export const allSettled = (promises) =>
+  promises
+    ? promises.reduce(
+        async (acc, promise) =>
+          (await acc).concat(await promise.catch((err) => err)),
+        Promise.resolve([])
+      )
+    : Promise.resolve();
+
+export const race = (promises) =>
+  promises
+    ? new Promise((resolve, reject) =>
+        promises.length
+          ? promises.forEach((promise) => promise.then(resolve, reject))
+          : resolve([])
+      )
+    : Promise.resolve();
+
+export const any = (promises) =>
+  promises
+    ? new Promise((resolve, reject) => {
+        promises.length
+          ? promises.forEach((promise) =>
+              promise.then(resolve).catch(() => null)
+            )
+          : resolve([]);
+        allSettled(promises).then(reject);
+      })
+    : Promise.resolve();
